162bef7e5a334237b3db7e83c978089fe1ba3a87
Nicolas Renard <87765071+namrenard@users.noreply.github.com>
2022-05-19 15:54:39 +0200

    v1 final commit

---
+++ executable/Lance_sudoku.bat
@@ -0,0 +1,14 @@
+REM --------------------------------
+REM  Sudoku_TP.bat
+REM --------------------------------
+REM	Lance le prgramme de résolution de sudoku
+REM --------------------------------
+REM - Version du JDK
+javac -version
+REM - Lancement du programme
+java -jar NFP136_Nicolas_RENARD.jar grille.txt
+
+pause
+
+echo "Faites 'entrer' pour clore le programme"
+:END
